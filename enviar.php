1088b7b0d0b0a44f3a2e7bdf0abb50bc99e89550
Jorge Puente <jorge.puente@ecstasy.cl>
2019-02-28 18:03:04 -0300

    Primner push

---
+++ enviar.php
@@ -0,0 +1,39 @@
+<?php
+   //Reseteamos variables a 0.
+   $nombre = $email = $numero = $company = $subject = $mensaje = $para = $headers = $msjCorreo = NULL;
+
+   if (isset($_POST['submit'])) {
+      //Obtenemos valores input formulario
+      $nombre = $_POST['name'];
+      $email = $_POST['email'];
+      $numero = $_POST['numero'];
+      $company = $_POST['company'];
+      $subject = $_POST['subject'];   
+      $mensaje = $_POST['message'];
+      $para = 'jorge.puente@ecstasy.cl';
+
+      //Creamos cabecera.
+      $headers = 'From' . " " . $email . "\r\n";
+      $headers .= "Content-type: text/html; charset=utf-8";
+
+      //Componemos cuerpo correo.
+      $msjCorreo = "Nombre: " . $nombre;
+      $msjCorreo .= "\r\n";
+      $msjCorreo .= "Email: " . $email;
+      $msjCorreo .= "\r\n";
+      $msjCorreo .= "Asunto: " . $subject;
+      $msjCorreo .= "\r\n";
+      $msjCorreo .= "Mensaje: " . $mensaje;
+      $msjCorreo .= "\r\n";
+
+    if (mail($para, $subject, $msjCorreo, $headers)) {
+         echo "<script language='javascript'>
+            alert('Mensaje enviado, muchas gracias.');
+         </script>";
+    } else {
+         echo "<script language='javascript'>
+            alert('fallado');
+         </script>";
+    }
+  }
+?>
